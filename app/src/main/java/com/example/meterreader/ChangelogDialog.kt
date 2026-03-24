408a0b6525c3aa3d06b0bead54957d12ef6cf147
tiktokfg957 <tiktokfg957@gmail.com>
2026-03-24 14:33:26 +0700

    Update ChangelogDialog.kt

--- app/src/main/java/com/example/meterreader/ChangelogDialog.kt
+++ app/src/main/java/com/example/meterreader/ChangelogDialog.kt
@@ -19,14 +19,14 @@ class ChangelogDialog : DialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         val btnClose = view.findViewById<Button>(R.id.btnChangelogClose)
         btnClose.setOnClickListener {
-            dismiss()
+            dismiss() // закрываем диалог
         }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return MaterialAlertDialogBuilder(requireContext())
             .setView(LayoutInflater.from(context).inflate(R.layout.dialog_changelog, null))
-            .setCancelable(true)
+            .setCancelable(true) // разрешаем закрытие по нажатию вне диалога
             .create()
     }
 }
