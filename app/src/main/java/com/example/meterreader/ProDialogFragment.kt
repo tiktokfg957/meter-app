a804a9524a35da699a058c75d30c8acf0058372e
tiktokfg957 <tiktokfg957@gmail.com>
2026-03-24 10:37:34 +0700

    Update ProDialogFragment.kt

--- app/src/main/java/com/example/meterreader/ProDialogFragment.kt
+++ app/src/main/java/com/example/meterreader/ProDialogFragment.kt
@@ -20,7 +20,6 @@ class ProDialogFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Кнопка теперь информационная, ничего не делает
         val btnInfo = view.findViewById<Button>(R.id.btnInfo)
         btnInfo.isEnabled = false
     }
