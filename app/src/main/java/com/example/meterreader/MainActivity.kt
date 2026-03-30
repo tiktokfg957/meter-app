37655e207d052ed67ec536666503579e3cd27d86
tiktokfg957 <tiktokfg957@gmail.com>
2026-03-30 22:58:44 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/meterreader/MainActivity.kt
+++ app/src/main/java/com/example/meterreader/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
