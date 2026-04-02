4fab7dd6d8533cdb0938ce4bc68174586b0cd0df
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-02 22:57:59 +0700

    Update SupportChatActivity.kt

--- app/src/main/java/com/example/meterreader/SupportChatActivity.kt
+++ app/src/main/java/com/example/meterreader/SupportChatActivity.kt
@@ -3,6 +3,7 @@ package com.example.meterreader
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
