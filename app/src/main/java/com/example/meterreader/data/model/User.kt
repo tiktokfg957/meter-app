7140fbd3c1ba6f0ac05ba032c247396b0f001ac9
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-02 16:53:57 +0700

    Create User.kt

---
+++ app/src/main/java/com/example/meterreader/data/model/User.kt
@@ -0,0 +1,12 @@
+package com.example.meterreader.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val isDefault: Boolean = false
+)
