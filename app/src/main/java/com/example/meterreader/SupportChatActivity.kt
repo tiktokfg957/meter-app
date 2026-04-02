61b93cd923c631ae82c5f5d18a1669609d376a78
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-02 23:04:21 +0700

    Update SupportChatActivity.kt

--- app/src/main/java/com/example/meterreader/SupportChatActivity.kt
+++ app/src/main/java/com/example/meterreader/SupportChatActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -106,7 +105,7 @@ class SupportChatActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
+        finishAfterTransition()   // вместо onBackPressed()
         return true
     }
 }
